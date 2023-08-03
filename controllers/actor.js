4f8a7047926b24cc60822bcc9a12a98bbbf0a16c
Adamya Singh <adamya45@gmail.com>
2023-08-03 20:32:13 +0530

    image optimization

--- controllers/actor.js
+++ controllers/actor.js
@@ -1,4 +1,6 @@
+const { isValidObjectId } = require("mongoose");
 const Actor = require("../models/actor");
+const { sendError } = require("../utils/helper");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -13,12 +15,60 @@ exports.createActor = async (req, res) => {
   const { file } = req;
 
   const newActor = new Actor({ name, about, gender });
-  /*const uploadRes = await cloudinary.uploader.upload(file.path);
-  console.log(uploadRes);
-  res.send("ok");*/
-  const { secure_url, public_id } = await cloudinary.uploader.upload(file.path);
-
-  newActor.avatar = { url: secure_url, public_id };
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path,
+      { gravity: "face", height: 500, width: 500, crop: "thumb" }
+    );
+    newActor.avatar = { url: secure_url, public_id };
+  }
   await newActor.save();
-  res.status(201).json(newActor);
+  res.status(201).json({
+    id: newActor._id,
+    name,
+    about,
+    gender,
+    avatar: newActor.avatar?.url,
+  });
+};
+
+exports.updateActor = async (req, res) => {
+  const { name, about, gender } = req.body;
+  const { file } = req;
+  const { actorId } = req.params;
+
+  if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!");
+  const actor = await Actor.findById(actorId);
+  if (!actor) return sendError(res, "Invalid request, record not found");
+
+  const public_id = actor.avatar?.public_id;
+
+  // remove old image if there was one!
+  if (public_id && file) {
+    const { result } = await cloudinary.uploader.destroy(public_id);
+    if (result !== "ok") {
+      return sendError(res, "Error in deleting old image!");
+    }
+  }
+
+  // upload new avatar if there is one!
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path,
+      { gravity: "face", height: 500, width: 500, crop: "thumb" }
+    );
+    actor.avatar = { url: secure_url, public_id };
+  }
+
+  actor.name = name;
+  actor.about = about;
+  actor.gender = gender;
+  await actor.save();
+  res.status(201).json({
+    id: actor._id,
+    name,
+    about,
+    gender,
+    avatar: actor.avatar?.url,
+  });
 };
